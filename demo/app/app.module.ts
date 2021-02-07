ab9969d0eac181c199e2f90eccabe0a4d69dc99d
mledour <michael.ledour@gmail.com>
2021-02-07 22:54:04 +0100

    feat: collapse and toggle icon animations

--- demo/app/app.module.ts
+++ demo/app/app.module.ts
@@ -3,18 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { SidebarMenuModule } from 'angular-sidebar-menu';
-import { AppRoutingModule } from './app-routing.module';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ConfigurationComponent } from './configuration/configuration.component';
 import { GetStartedComponent } from './get-started/get-started.component';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, ConfigurationComponent, GetStartedComponent],
-  imports: [BrowserModule, MatSidenavModule, AppRoutingModule, SidebarMenuModule, NoopAnimationsModule],
+  imports: [BrowserModule, BrowserAnimationsModule, MatSidenavModule, AppRoutingModule, SidebarMenuModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- library/src/lib/sidebar-menu-item.component.ts
+++ library/src/lib/sidebar-menu-item.component.ts
@@ -1,17 +1,40 @@
-import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { AfterViewInit, Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
 import { Event as RouterEvent, NavigationEnd, Router, RouterLinkActive } from '@angular/router';
+import { trigger, state, style, animate, transition, AUTO_STYLE, AnimationEvent, group, keyframes } from '@angular/animations';
+
 import { Subject } from 'rxjs';
-
 import { filter, takeUntil } from 'rxjs/operators';
-import { SidebarMenuItemService } from './sidebar-menu-item.service';
 
+import { SidebarMenuItemService } from './sidebar-menu-item.service';
 import { MenuItem } from './sidebar-menu.interface';
+
+const TRANSITION_DURATION = 300;
 
 @Component({
   // tslint:disable-next-line:component-selector
   selector: 'li[asm-sidebar-menu-item]',
   templateUrl: 'sidebar-menu-item.component.html',
   styleUrls: ['sidebar-menu-item.component.scss'],
+  animations: [
+    trigger('openClose', [
+      state('true', style({ height: AUTO_STYLE })),
+      state('false', style({ height: 0, overflow: 'hidden' })),
+      transition(':enter', []),
+      transition('false => true', animate(`${TRANSITION_DURATION}ms ease-in`, style({ height: AUTO_STYLE }))),
+      transition(
+        'true => false',
+        group([
+          animate(`${TRANSITION_DURATION}ms ease-in`, style({ height: 0 })),
+          animate(`${TRANSITION_DURATION}ms steps(1, start)`, style({ overflow: 'hidden' })),
+        ])
+      ),
+    ]),
+    trigger('rotate', [
+      state('true', style({ transform: 'rotate(-90deg)' })),
+      transition(':enter', []),
+      transition('false <=> true', animate(`${TRANSITION_DURATION}ms ease-out`)),
+    ]),
+  ],
 })
 export class SidebarMenuItemComponent implements OnInit, OnDestroy {
   // tslint:disable-next-line:no-input-rename
@@ -22,8 +45,10 @@ export class SidebarMenuItemComponent implements OnInit, OnDestroy {
 
   @Output() isItemActive = new EventEmitter<boolean>();
 
-  isOpen = false;
+  isOpen?: boolean;
   isActiveChild = false;
+  disableAnimation = true;
+  isAnimating = false;
 
   private onDestroy$ = new Subject();
 
@@ -31,9 +56,14 @@ export class SidebarMenuItemComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     if (this.menuItem.route) {
-      this.router.events.pipe(filter((e: RouterEvent): e is NavigationEnd => e instanceof NavigationEnd)).subscribe(() => {
-        this.isItemActive.emit(this.router.isActive(this.menuItem.route as string, true));
-      });
+      this.router.events
+        .pipe(
+          filter((e: RouterEvent): e is NavigationEnd => e instanceof NavigationEnd),
+          takeUntil(this.onDestroy$)
+        )
+        .subscribe(() => {
+          this.isItemActive.emit(this.router.isActive(this.menuItem.route as string, true));
+        });
     }
 
     if (this.menuItem.children) {
